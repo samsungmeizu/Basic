0b3d7f1aa04ddf247ecbab5564e67ecc037598aa
pyaiveoleg <pyaive.oleg@yandex.ru>
2020-01-20 18:56:59 +0300

    First commit

---
+++ Task1/cmake-build-debug/CMakeFiles/Task1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task1.dir/main.c.o"
+  "Task1.pdb"
+  "Task1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Task1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
